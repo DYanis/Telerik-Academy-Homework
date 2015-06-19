7097f01a4a4a9662a5387afaf4b8d1380d6fb32d
DYanis <dimitar.yanis@yahoo.com>
2015-06-19 19:56:38 +0300

    add HW

---
+++ High-Quality-Code/01.Code Formatting/1. Formatting C#/formatingProblem/EventHolder.cs
@@ -0,0 +1,25 @@
+﻿namespace Wintellect
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+   public class EventHolder
+    {
+        internal void ListEvents(DateTime date, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void DeleteEvents(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddEvent(DateTime date, string title, string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ High-Quality-Code/01.Code Formatting/1. Formatting C#/formatingProblem/OrderedBag.cs
@@ -0,0 +1,22 @@
+﻿namespace Wintellect.PowerCollections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class OrderedBag<T>
+    {
+        public static object VieweventsToShow { get; set; }
+
+        internal void Add(Event newEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object RangeFrom(Event p1, bool p2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
